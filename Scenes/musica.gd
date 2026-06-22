941f092761494d26f1be7c019707973158243a68
Andres Felipe Rojas <107456253+Bakudi@users.noreply.github.com>
2026-06-22 16:40:30 -0500

    add: musica funcional, mantenimiento entre escenas y medidor de volumen interno

---
+++ Scenes/musica.gd
@@ -0,0 +1,15 @@
+extends AudioStreamPlayer
+
+const musica = preload("res://Assets/Bakudi test/The Amazing Digital Circus - Main Theme.mp3")
+
+func _play_music(music: AudioStream, volume = 0.0):
+	if stream == music:
+		return
+	
+	stream = music
+	volume_db = volume
+	play()
+
+
+func play_music_level():
+	_play_music(musica)
---
+++ Scenes/opciones.gd
@@ -0,0 +1,19 @@
+extends Control
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),linear_to_db(value))
+
+
+func _on_atras_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
