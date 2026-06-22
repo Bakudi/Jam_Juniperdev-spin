bded2fd0668390018730847fb8fd3b66f8d5ff03
Andres Felipe Rojas <107456253+Bakudi@users.noreply.github.com>
2026-06-22 18:13:47 -0500

    add: rediseño de las opciones, resolucion variable y funcional

--- Scenes/opciones.gd
+++ Scenes/opciones.gd
@@ -17,3 +17,25 @@ func _on_h_slider_value_changed(value: float) -> void:
 
 func _on_atras_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	
+func _on_option_button_item_selected(index: int) -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+	
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(800, 600))
+		1:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+		2:
+			DisplayServer.window_set_size(Vector2i(1600, 900))
+		3:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		4:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		5:
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+			DisplayServer.window_set_size(DisplayServer.screen_get_size())
+			DisplayServer.window_set_position(Vector2i.ZERO)
+		6:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
