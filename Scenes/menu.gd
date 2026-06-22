72e8b742a15ddd21696f21720a98457893220c9f
Andres Felipe Rojas <107456253+Bakudi@users.noreply.github.com>
2026-06-21 22:48:09 -0500

    add: menu y conexion de botones (configuraciones pendientes)

---
+++ Scenes/menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_jugar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Player/node_2d.tscn")
+
+
+func _on_configuraciones_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Opciones.tscn")
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
